90d53151265758b07ec2b14c2dc720b497656b7c
Alec Bargher <abargher@uchicago.edu>
2024-06-30 13:35:13 -0700

    Added automatic configuration setup and tmux.conf

---
+++ configs/README.md
@@ -0,0 +1,8 @@
+# Config Setup
+
+Before running the `setup-config.sh` script, please view it and make changes
+to suit your configs.
+
+Particularly, make sure to change the `CS_ROOT` variable if you have cloned
+this repository in a different location than the standard `~/CMSC/cs-tools`.
+
---
+++ configs/setup-config.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env zsh
+
+# Script assumes that the cs-tools repo is located at ~/${CS_ROOT}/cs-tools
+# Change this variable if you have cloned the repo elsewhere
+CS_ROOT=CMSC
+
+# save original directory
+ORIG_DIR=$(pwd)
+
+# change to cs-tools dir
+cd "/$HOME/$CS_ROOT/cs-tools"
+
+# copy configs to home directory, backing up old files if needed
+HOME_CONFIGS=(
+    zshrc
+    tmux.conf
+    vimrc
+)
+for conf in "${HOME_CONFIGS[@]}"; do
+    if test -f $HOME/.$conf; then
+        cp $HOME/.$conf $HOME/.$conf-$(TZ='America/Chicago' date "+%y-%m-%d_%H-%M").bak
+    fi
+    cp ./$conf /$HOME/.$conf
+done
+
+# return to original directory
+cd $ORIG_DIR
+
