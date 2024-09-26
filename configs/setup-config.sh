ce3731ca61c2ea17d07cd8af99eebc7d50eb2763
Alec Bargher <abargher@uchicago.edu>
2024-09-26 15:43:56 -0500

    alec: setup-config.sh update with symlinks

--- configs/setup-config.sh
+++ configs/setup-config.sh
@@ -8,7 +8,7 @@ CS_ROOT=CMSC
 ORIG_DIR=$(pwd)
 
 # change to cs-tools dir
-cd "/$HOME/$CS_ROOT/cs-tools"
+pushd "/$HOME/$CS_ROOT/cs-tools"
 
 # copy configs to home directory, backing up old files if needed
 HOME_CONFIGS=(
@@ -20,9 +20,9 @@ for conf in "${HOME_CONFIGS[@]}"; do
     if test -f $HOME/.$conf; then
         cp $HOME/.$conf $HOME/.$conf-$(TZ='America/Chicago' date "+%y-%m-%d_%H-%M").bak
     fi
-    cp /$HOME/$CS_ROOT/cs-tools/configs/$conf /$HOME/.$conf
+    ln -s /$HOME/$CS_ROOT/cs-tools/configs/$conf /$HOME/.$conf
 done
 
 # return to original directory
-cd $ORIG_DIR
+popd
 
