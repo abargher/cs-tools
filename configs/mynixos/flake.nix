283c29e9e02358d07568610414f75983fbe3c747
Alec Bargher <abargher@uchicago.edu>
2024-09-25 23:09:08 -0500

    adding eris thinkpad

--- configs/mynixos/flake.nix
+++ configs/mynixos/flake.nix
@@ -4,10 +4,12 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
   outputs = { nixpkgs, ... } @ inputs:
   {
+    # Einstein desktop
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
       modules = [
@@ -15,12 +17,20 @@
       ];
     };
     
+    # Thinkpad T480S
+    nixosConfigurations.eris = nixpkgs.lib.nixosSystem {
+      specialArgs = { inherit inputs; };
+      modules = [
+        nixos-hardware.nixosModules.lenovo-thinkpad-t480s
+        ./hosts/eris/configuration.nix
+      ];
+    };
+
     nixosConfigurations.orcus = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
       modules = [
         ./hosts/orcus/configuration.nix
       ];
     };
-
   };
 }
